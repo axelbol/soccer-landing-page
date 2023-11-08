81b0b588abfacb75635273c634c00701c24589ee
axelbol <axel_lp_alvarado@hotmail.com>
2023-11-07 21:26:39 -0400

    players blur & transitions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.css'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <!-- Styles -->
 
     <body class="antialiased bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#131313] via-zinc-900 to-zinc-950">
@@ -65,3 +65,43 @@
         <footer></footer>
     </body>
 </html>
+
+<script>
+    const $players = document.querySelector("#players")
+    const $playersImages = $players.querySelectorAll("img")
+
+    $playersImages.forEach(($img) => {
+        $img.addEventListener("mouseleave", () => {
+            $playersImages.forEach(($img) => {
+                $img.setAttribute("style", "transition: all 1s ease-in-out")
+            })
+        })
+
+        $img.addEventListener("mouseenter", () => {
+            const sectionImages = $img.parentNode.children
+            const position = Array.from(sectionImages).indexOf($img)
+            // left images and right images
+            const [leftPlayer, rightPlayer] = [
+                $playersImages[position], // left side
+                $playersImages[position + $playersImages.length / 2],
+            ]
+            // console.log(leftPlayer, rightPlayer)
+            // hide players not included
+            $playersImages.forEach(($img) => {
+                $img.setAttribute(
+                    "style",
+                    "filter: blur(2px) grayscale(.8); transition: all 1s ease-in-out"
+                )
+            })
+
+            leftPlayer.setAttribute(
+                "style",
+                "scale: 1.05; contrast: 1.1; transition: all .3s ease-in-out"
+            )
+            rightPlayer.setAttribute(
+                "style",
+                "scale: 1.05; contrast: 1.1; transition: all .3s ease-in-out"
+            )
+        })
+    })
+</script>
